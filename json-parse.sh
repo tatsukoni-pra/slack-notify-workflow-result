f9aca48a7d2e14a074b672f22b9db4fe339c4621
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2024-05-19 22:01:52 +0900

    改修

--- json-parse.sh
+++ json-parse.sh
@@ -1,6 +1,31 @@
 #!/bin/sh
 set -e
 
-cat sample.json | jq -c '.[] | .result' | while read -r result; do
-  echo "結果: $result"
+json='
+{
+  "job-1": {
+    "result": "success",
+    "outputs": {}
+  },
+  "job-2": {
+    "result": "success",
+    "outputs": {}
+  },
+  "job-3": {
+    "result": "success",
+    "outputs": {}
+  }
+}
+'
+
+echo $json | jq -c '.[] | .result' | while read -r result; do
+  echo $result
+  if [ "$result" = '"success"' ]; then
+    echo "成功"
+    break
+  fi
 done
+
+# cat sample.json | jq -c '.[] | .result' | while read -r result; do
+#   echo "結果: $result"
+# done
