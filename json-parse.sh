c66870f49d3ba4bb321c40803e09b246688b23e7
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2024-05-20 00:42:32 +0900

    改修

--- json-parse.sh
+++ json-parse.sh
@@ -1,30 +1,23 @@
 #!/bin/sh
 set -e
 
-json='
-{
-  "job-1": {
-    "result": "success",
-    "outputs": {}
-  },
-  "job-2": {
-    "result": "success",
-    "outputs": {}
-  },
-  "job-3": {
-    "result": "success",
-    "outputs": {}
-  }
-}
-'
+json='{n "job-a": {n "result": "success",n "outputs": {}n },n "job-b": {n "result": "failure",n "outputs": {}n },n "job-c": {n "result": "skipped",n "outputs": {}n }n}'
+fixed_json=$(echo "$json" | sed 's/n//g')
 
-echo $json | jq -c '.[] | .result' | while read -r result; do
-  echo $result
-  if [ "$result" = '"success"' ]; then
-    echo "成功"
+echo $fixed_json | jq -c '.[] | .result' | while read -r result; do
+  if [ "$result" != '"success"' ]; then
+    echo "job failure" > failure.txt
     break
   fi
 done
+
+if [ -e failure.txt ]; then
+  echo "job failure"
+else
+  echo "job success"
+fi
+
+rm -f failure.txt
 
 # cat sample.json | jq -c '.[] | .result' | while read -r result; do
 #   echo "結果: $result"
